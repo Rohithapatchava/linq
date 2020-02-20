beef8d0fb8771bd3c75fc1920648a5d8d07b4b4e
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-20 16:21:29 +0530

    Initial commit

---
+++ linq/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    class Course
+    {
+  
+
+
+        public int Cid { get; set; }
+        public string Cname { get; set; }
+        public int Fees { get; set; }
+            public DateTime StartDate { get; set; }
+    }
+}
---
+++ linq/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Course> clst = new List<Course>()
+            {
+                new Course { Cid = 1, Cname = "c#", Fees = 7000, StartDate = Convert.ToDateTime("10-02-2020") },
+                new Course
+                {
+                    Cid = 2,
+                    Cname = "javascript",
+                    Fees = 8000,
+                    StartDate = Convert.ToDateTime("9-02-2020")
+                },
+                new Course
+                {
+                    Cid = 3,
+                    Cname = "java",
+                    Fees = 9000,
+                    StartDate = Convert.ToDateTime("7-02-2020")
+                },
+                new Course
+                {
+                    Cid = 4,
+                    Cname = "c",
+                    Fees = 7000,
+                    StartDate = Convert.ToDateTime("3-02-2020")
+                },
+            };
+            var query = from gq in clst
+                        where gq.Cid == 3
+                        select gq;
+            foreach (var q in query)
+            {
+
+                Console.WriteLine("{0}\n{1}\n{2}\n{3}", q.Cid, q.Cname, q.StartDate, q.Fees);
+            }
+            var qget = from a in clst
+                       orderby a.Fees
+                       select a;
+            foreach (var q in qget)
+            {
+                Console.WriteLine("{0}\n{1}\n{2}\n{3}", q.Cid, q.Cname, q.StartDate, q.Fees);
+            }
+        }
+    }
+}
+
